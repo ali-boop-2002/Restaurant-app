de72c5946092b6dc5e0b866967cf1807dbf0baf9
Ali Shahid <ali@MacBookAir.lan>
2025-08-27 02:09:29 -0400

    admin side

--- app/api/items/route.js
+++ app/api/items/route.js
@@ -2,7 +2,11 @@ import connectDB from "@/config/database";
 import Items from "@/models/Item";
 
 export async function GET(req, res) {
-  await connectDB();
-  console.log(req);
-  return Response.json(req);
+  try {
+    await connectDB();
+    const data = Items.find({}).lean();
+    return Response.json(data);
+  } catch (err) {
+    console.error(err);
+  }
 }
